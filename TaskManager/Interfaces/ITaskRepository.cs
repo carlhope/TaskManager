67c9c5c2332c2315be132b9cef0d1ec85d759dc6
Carl hope <84421131+carlhope@users.noreply.github.com>
2025-08-21 15:40:56 +0100

    Create ITaskRepository

---
+++ TaskManager/Interfaces/ITaskRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Models;
+
+namespace TaskManager.Interfaces
+{
+    public interface ITaskRepository
+    {
+        List<TaskItem> LoadTasks();
+        void SaveTasks(List<TaskItem> tasks);
+    }
+}
