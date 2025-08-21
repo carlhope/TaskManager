63193bab15e09e6ada475ae28f15a255fc44f9e3
Carl hope <84421131+carlhope@users.noreply.github.com>
2025-08-21 15:42:16 +0100

    Create TaskRepository

---
+++ TaskManager/Repositories/TaskRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Interfaces;
+using TaskManager.Models;
+
+namespace TaskManager.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+        public List<TaskItem> LoadTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveTasks(List<TaskItem> tasks)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
