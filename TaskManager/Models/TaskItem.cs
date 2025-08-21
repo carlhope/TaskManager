0210741fbdeaa68adde8bdd6aeb47cdde2f49cf4
Carl hope <84421131+carlhope@users.noreply.github.com>
2025-08-21 15:39:07 +0100

    Create TaskItem model

---
+++ TaskManager/Models/TaskItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Models
+{
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High
+    }
+    public class TaskItem
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateOnly DueDate { get; set; }
+        public bool IsComplete { get; set; } = false;
+        public Priority Priority { get; set; } = Priority.Medium;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    }
+}
